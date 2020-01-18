dd4159281b915c26c577f09398431bc7c5b8eea6
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-19 00:50:03 +0900

    breadth first search algorithm implementation

--- BreadthFirstSearch/main.cpp
+++ BreadthFirstSearch/main.cpp
@@ -4,12 +4,46 @@
 //
 //  Created by adeeb mohammed on 15/01/20.
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
-//
+// breadth first search algorithm,
+// based on queue a node and its child is inserted into the array
+// here the class nodes contains the tree
+// the time complexity of the algorithm is o(v+e) the space complexity is o(v)
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <deque>
+
+class Node {
+public:
+  std::string name;
+  std::vector<Node *> children;
+  Node(std::string str) { name = str; }
+
+  std::vector<std::string> breadthFirstSearch(std::vector<std::string> *array) {
+      std::deque<Node *> queue{this};
+      while(!queue.empty()){
+          Node current = *queue.front();
+          queue.pop_front();
+          array->push_back(current.name);
+          for(int i = 0; i < current.children.size(); i++){
+              queue.push_back(current.children[i]);
+          }
+      }
+    
+      return *array;
+  }
+
+  Node *addChild(std::string name) {
+    Node *child = new Node(name);
+    children.push_back(child);
+    return this;
+  }
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::cout << "BreadFirst Search Algorithm implemented\n";
     return 0;
 }
